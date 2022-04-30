8bd2275268ad68f386df4c7a582f9c7b98350fcf
Thomas Mansencal <thomas.mansencal@gmail.com>
2022-04-30 22:27:08 +1200

    Raise app version to 0.1.14.

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ __application_name__ = "Colour - Dash"
 
 __major_version__ = "0"
 __minor_version__ = "1"
-__change_version__ = "13"
+__change_version__ = "14"
 __version__ = ".".join(
     (__major_version__, __minor_version__, __change_version__)
 )
